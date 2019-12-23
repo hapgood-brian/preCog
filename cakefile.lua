3da6fda9738a785485b9f1bab736d41aacd220ac
Brian Hapgood <brian.pikaboo@gmail.com>
2019-12-22 23:22:19 -0600

    Now flushing out the platform generators.

--- cakefile.lua
+++ cakefile.lua
@@ -9,16 +9,6 @@
 
 if platform:is'apple'then
   print'Constructing eon workspace for Xcode'
-  local eon = workspace:declare'eon'--> Will create eon.xcworkspace
-  local lib = eon:declare'eon'--> Will create eon.xcproj
-  lib : include'usr/engine/include'
-      : sources'usr/engine/src'
-      : target'framework'
-  if platform.save( eon, 'tmp' )then
-    return true
-  end
-  error"Couldn't save to platform."
-  return false
 end
 
 --------------------------------------------------------------------------------
@@ -27,12 +17,19 @@ end
 
 if platform.is'win64'then
   print'Constructing eon workspace for Visual Studio'
-  return true
 end
 
 --------------------------------------------------------------------------------
--- Just print an error!
+-- Build the project cross platform and save.
 --------------------------------------------------------------------------------
 
-print'Unknown platform'
+local eon = workspace:declare'eon'--> Will create eon.xcworkspace
+local lib = eon:declare'eon'--> Will create eon.xcproj
+lib : include'usr/engine/include'
+    : sources'usr/engine/src'
+    : target'framework'
+if platform.save( eon, 'tmp' )then
+  return true
+end
+error"Couldn't save to platform."
 return false
