271905a2bb67cfbcdcedc5159440d0a141b789ee
Brian Hapgood <brian.pikaboo@gmail.com>
2020-01-03 18:17:42 -0600

    New Lua code.

--- cogfile.lua
+++ cogfile.lua
@@ -2,8 +2,8 @@
 -- Create a new workspace.
 --------------------------------------------------------------------------------
 
-local project = workspace:new'cog'
-local BUILD_FRAMEWORK = false
+local project      = workspace:new'cog'
+local useFramework = false
 
 --------------------------------------------------------------------------------
 -- Create a new project under workspace to compile startup code.
@@ -23,7 +23,7 @@ project:new'startup'
 -- Setup the build settings for engine build (xcode)
 --------------------------------------------------------------------------------
 
-if BUILD_FRAMEWORK then project:new'gfc'
+if useFramework then project:new'gfc'
   : defines(
     '_DEBUG=1, DEBUG=1',
     'NDEBUG=1' )
@@ -63,10 +63,10 @@ project:new'pal'
     '_DEBUG=1, DEBUG=1',
     'NDEBUG=1' )
   : set_include_paths'/usr/local/include'
-  : find_includes    'src/pal/include'
-  : find_sources     'src/pal/src/${PLATFORM}'
-  : prefix           'src/engine/include/xcode-prefix.pch'
-  : target           'static'
+  : find_includes'src/pal/include'
+  : find_sources'src/pal/src/${PLATFORM}'
+  : prefix'src/engine/include/xcode-prefix.pch'
+  : target'static'
 
 --------------------------------------------------------------------------------
 -- Lua library.
@@ -77,40 +77,62 @@ project:new'lua'
     '_DEBUG=1, DEBUG=1',
     'NDEBUG=1' )
   : set_include_paths'/usr/local/include'
-  : find_includes    'src/lua/5.3.5/include'
-  : find_sources     'src/lua/5.3.5/src'
-  : target           'static'
+  : find_includes'src/lua/5.3.5/include'
+  : find_sources'src/lua/5.3.5/src'
+  : target'static'
 
 --------------------------------------------------------------------------------
 -- Create a new project under workspace to compile application.
 --------------------------------------------------------------------------------
 
-if BUILD_FRAMEWORK then project:new'cog'
+if useFramework then project:new'cog'
   : defines(
     '_DEBUG=1, DEBUG=1',
     'NDEBUG=1' )
-  : install
-    "[ ${CONFIGURATION} == 'Release' ] && { cp ${TARGET_BUILD_DIR}/${TARGET_NAME} /usr/local/bin }"
+  : install[[
+      [ "${CONFIGURATION}" == 'Debug' ] && {
+        rm "../rel/macOS/${CONFIGURATION}/${TARGET_NAME}.a" 2> /dev/null
+        [ cp "${TARGET_BUILD_DIR}/${TARGET_NAME}.a" "../rel/macOS/${CONFIGURATION}" 2> /dev/null ] && {
+          echo "Copied product!"
+        }
+      } || [ "${CONFIGURATION}" == 'Release' ] && {
+        rm "../rel/macOS/${CONFIGURATION}/${TARGET_NAME}.a" 2> /dev/null
+        [ cp "${TARGET_BUILD_DIR}/${TARGET_NAME}.a" "../rel/macOS/${CONFIGURATION}" 2> /dev/null ] && {
+          echo "Copied product!"
+        }
+      }
+      exit 0
+    ]]
   : set_include_paths'src/app/include,/usr/local/include'
-  : find_includes    'src/app/include'
-  : find_sources     'src/app/src'
-  : link_with        'gfc.framework,libstartup.a,liblua.a'
-  : prefix           'src/engine/include/xcode-prefix.pch'
-  : target           'console'
+  : find_includes'src/app/include'
+  : find_sources'src/app/src'
+  : link_with'gfc.framework,libstartup.a,liblua.a'
+  : prefix'src/engine/include/xcode-prefix.pch'
+  : target'console'
 else project:new'cog'
   : defines(
     '_DEBUG=1, DEBUG=1',
     'NDEBUG=1' )
   : install[[
-    [ ${CONFIGURATION} == 'Release' ] && {
-      cp ${TARGET_BUILD_DIR}/${TARGET_NAME} /usr/local/bin
-    }]]
+      [ "${CONFIGURATION}" == 'Debug' ] && {
+        rm "../rel/macOS/${CONFIGURATION}/${TARGET_NAME}.a" 2> /dev/null
+        [ cp "${TARGET_BUILD_DIR}/${TARGET_NAME}.a" "../rel/macOS/${CONFIGURATION}" 2> /dev/null ] && {
+          echo "Copied product!"
+        }
+      } || [ "${CONFIGURATION}" == 'Release' ] && {
+        rm "../rel/macOS/${CONFIGURATION}/${TARGET_NAME}.a" 2> /dev/null
+        [ cp "${TARGET_BUILD_DIR}/${TARGET_NAME}.a" "../rel/macOS/${CONFIGURATION}" 2> /dev/null ] && {
+          echo "Copied product!"
+        }
+      }
+      exit 0
+    ]]
   : set_include_paths'src/app/include,/usr/local/include'
-  : find_includes    'src/app/include'
-  : find_sources     'src/app/src'
-  : link_with        'libboost_filesystem.a,liblz4.a,Foundation.framework,libpal.a,libgfc.a,libstartup.a,liblua.a'
-  : prefix           'src/engine/include/xcode-prefix.pch'
-  : target           'console'
+  : find_includes'src/app/include'
+  : find_sources'src/app/src'
+  : link_with'libboost_filesystem.a,liblz4.a,Foundation.framework,libpal.a,libgfc.a,libstartup.a,liblua.a'
+  : prefix'src/engine/include/xcode-prefix.pch'
+  : target'console'
 end
 
 --------------------------------------------------------------------------------
