c0e46094192b312609b7afd9d7f8e47286cb77ef
Brian Hapgood <brian.pikaboo@gmail.com>
2020-01-30 15:06:36 -0600

    New version.

--- src/app/src/main.cpp
+++ src/app/src/main.cpp
@@ -226,7 +226,7 @@ using namespace gfc;
 //------------------------------------------------|-----------------------------
 
 int IEngine::main( const strings& args ){
-  e_msgf( "Cog build system v1.1.9" );
+  e_msgf( "Cog build system v1.2.0" );
   #if e_compiling( osx )
     Workspace::bmp->bXcode11 = 1;
   #elif e_compiling( microsoft )
