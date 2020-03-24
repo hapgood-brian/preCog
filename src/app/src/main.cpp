ee0880e0c8fc9ab36d1c6112eabfa4a09a625fa3
Brian Hapgood <brian.pikaboo@gmail.com>
2020-03-23 19:13:13 -0500

    Updating the version number.

--- src/app/src/main.cpp
+++ src/app/src/main.cpp
@@ -243,7 +243,7 @@ using namespace gfc;
 
 int IEngine::main( const strings& args ){
   // Odd versions are bug fix releases.
-  e_msgf( "Cog build system v1.2.3a" );
+  e_msgf( "Cog build system v1.2.3b" );
   #if e_compiling( osx )
     Workspace::bmp->bXcode11 = 1;
   #elif e_compiling( microsoft )
