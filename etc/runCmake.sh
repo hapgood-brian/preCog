2d323577796cabc2dede5035e881556651abc99b
virtua <brian.pikaboo@gmail.com>
2020-09-20 21:31:29 -0500

    Updated cmake scripts.

--- etc/runCmake.sh
+++ etc/runCmake.sh
@@ -1,8 +1,10 @@
 OS=`uname`
 [ "$OS" == 'Darwin' ] && {
   cmake .. -G Xcode -Wno-dev
-} || [ "$OS" == 'Linux' ] && {
-  cmake .. -G "Unix Makefiles" -Wno-dev
-} || {
+}
+[ "$OS" == 'Linux' ] && {
+  cmake .. -G "CodeLite - Ninja" -Wno-dev
+}
+[ "$OS" == "Windows" ] && {
   cmake .. -G "Visual Studio 16 2019" -Wno-dev
 }
