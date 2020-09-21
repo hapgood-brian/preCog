e8844aff7bfcb2ff67ae425d15b4b4b7830ebfcb
virtua <brian.pikaboo@gmail.com>
2020-09-20 21:23:43 -0500

    Added UNIX.

--- etc/runCmake.sh
+++ etc/runCmake.sh
@@ -1,6 +1,8 @@
 OS=`uname`
 [ "$OS" == 'Darwin' ] && {
   cmake .. -G Xcode -Wno-dev
+} || [ "$OS" == 'Linux' ] && {
+  cmake .. -G "Unix Makefiles" -Wno-dev
 } || {
   cmake .. -G "Visual Studio 16 2019" -Wno-dev
 }
