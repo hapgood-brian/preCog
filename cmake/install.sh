426ecf340c3a1cc96dc370c95934f35ee7fcf134
Brian Hapgood <brian.pikaboo@gmail.com>
2020-09-23 18:10:58 -0500

    Updated install.sh to copy to /usr/local/bin

--- cmake/install.sh
+++ cmake/install.sh
@@ -3,11 +3,13 @@ OS=`uname`
 
 [ "$OS" == 'Darwin' ] && {
   cp src/applications/cog ../bin/macOS/
+  sudo cp ../bin/macOS/cog /usr/local/bin
   exit 0
 }
 
 [ "$OS" == 'Linux' ] && {
   cp src/applications/cog ../bin/linux/
+  sudo cp ../bin/linux/cog /usr/local/bin
   exit 0
 }
 
