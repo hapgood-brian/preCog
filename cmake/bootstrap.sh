e662e511d0bc0c05644459c811da2641e0bed741
hapgood-brian <brian.pikaboo@gmail.com>
2020-09-24 10:37:53 -0500

    There can be only one.

--- cmake/bootstrap.sh
+++ cmake/bootstrap.sh
@@ -42,6 +42,28 @@ echo "$OS"
     rm -rf x64
     rm -rf .vs
   }
+  exit 0
+}
+
+#-------------------------------------------------------------------------------
+# Installation.
+#-------------------------------------------------------------------------------
+
+[ "$1" = '--install' ] && {
+  [ "$OS" == 'Darwin' ] && {
+    cp src/applications/cog ../bin/macOS/
+    sudo cp ../bin/macOS/cog /usr/local/bin
+    exit 0
+  }
+
+  [ "$OS" == 'Linux' ] && {
+    cp src/applications/cog ../bin/linux/
+    sudo cp ../bin/linux/cog /usr/local/bin
+    exit 0
+  }
+
+  cp src/applications/Release/cog.exe ../bin/win64
+  exit 0
 }
 
 #-------------------------------------------------------------------------------
