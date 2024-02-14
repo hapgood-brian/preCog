d99b43dbbf41b4222cd2bb103f6603d49c836651
Lord Shader <creepydollsoft@gmail.com>
2024-02-14 17:59:09 -0600

    Updated Win64 log.cpp

--- src/pal/src/win/log.cpp
+++ src/pal/src/win/log.cpp
@@ -1,57 +1,33 @@
 //------------------------------------------------------------------------------
-//       Copyright 2015-2016 Superluminal Games LLC. All rights reserved.
+//                   Copyright 2022 Creepy Doll Software, Inc.
+//                            All rights reserved.
 //
 //                  The best method for accelerating a computer
 //                     is the one that boosts it by 9.8 m/s2.
 //------------------------------------------------------------------------------
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY EXPRESS
-// OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN
-// NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
-// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//------------------------------------------------------------------------------
 
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif
-#ifndef WIN32_EXTRA_LEAN
-#define WIN32_EXTRA_LEAN
-#endif
-#ifndef NOMINMAX
-#define NOMINMAX
-#endif
-#ifndef VC_EXTRALEAN
-#define VC_EXTRALEAN
-#endif
-#include<windows.h>
-#include<eon/eon.h>
+#define BOOST_STACKTRACE_GNU_SOURCE_NOT_REQUIRED
+#import<boost/stacktrace.hpp>
+#import<Cocoa/Cocoa.h>
+#import<execinfo.h>
 
 using namespace EON;
 using namespace gfc;
+
+#define SALLOC_SIZE 4096
 
 #undef e_halt
 #undef e_longlogf
 #undef e_longlog
 #undef e_logf
 #undef e_log
-#undef e_brk
+#undef e_break
 
-//================================================|=============================
+//================================================+=============================
 //Private:{                                       |
+  //[logging]:{                                   |
 
-  namespace{
-
-    std::mutex& mutie(){
-      static std::mutex m;
-      return m;
-    }
-
-    void translateCrtColors( string& s ){
+    string& string::asCLI( string& s ){
       //http://tldp.org/HOWTO/Bash-Prompt-HOWTO/x329.html
       s.replace( "$(black)",       "\033[0;30m" );
       s.replace( "$(red)",         "\033[0;31m" );
@@ -70,244 +46,298 @@ using namespace gfc;
       s.replace( "$(lightcyan)",   "\033[1;36m" );
       s.replace( "$(white)",       "\033[1;37m" );
       s.replace( "$(off)",         "\033[0m"    );
+      return s;
     }
 
-    void stripCrtColors( string& s ){
-      s.replace( "$(black)",       "" );
-      s.replace( "$(red)",         "" );
-      s.replace( "$(green)",       "" );
-      s.replace( "$(brown)",       "" );
-      s.replace( "$(blue)",        "" );
-      s.replace( "$(purple)",      "" );
-      s.replace( "$(cyan)",        "" );
-      s.replace( "$(lightgrey)",   "" );
-      s.replace( "$(darkgrey)",    "" );
-      s.replace( "$(lightred)",    "" );
-      s.replace( "$(lightgreen)",  "" );
-      s.replace( "$(yellow)",      "" );
-      s.replace( "$(lightblue)",   "" );
-      s.replace( "$(lightpurple)", "" );
-      s.replace( "$(lightcyan)",   "" );
-      s.replace( "$(white)",       "" );
-      s.replace( "$(off)",         "" );
+    string& string::strip( string& s ){
+      asCLI( s                  );
+      s.erase( "\033[0;30m"     );
+      s.erase( "\033[0;31m"     );
+      s.erase( "\033[0;32m"     );
+      s.erase( "\033[0;33m"     );
+      s.erase( "\033[0;34m"     );
+      s.erase( "\033[0;35m"     );
+      s.erase( "\033[0;36m"     );
+      s.erase( "\033[0;37m"     );
+      s.erase( "\033[1;30m"     );
+      s.erase( "\033[1;31m"     );
+      s.erase( "\033[1;32m"     );
+      s.erase( "\033[1;33m"     );
+      s.erase( "\033[1;34m"     );
+      s.erase( "\033[1;35m"     );
+      s.erase( "\033[1;36m"     );
+      s.erase( "\033[1;37m"     );
+      s.erase( "\033[0m"        );
+      return s;
     }
-  }
 
+  //}:                                            |
 //}:                                              |
-//e_warnsf:{                                      |
+//================================================+=============================
+//                                                :
+//                                                :
+//                                                :
+//================================================+=============================
+//Methods:{                                       |
+  //e_stktrc:{                                    |
 
-  s32 e_warnsf( ccp format,... ){
-    va_list va;
-    va_start( va, format );
-      const s32 r = e_warnsv( format, va );
-    va_end( va );
-    return r;
-  }
-
-//}:                                              |
-//e_warnsv:{                                      |
-
-  s32 e_warnsv( ccp format, va_list va ){
-
-    //--------------------------------------------------------------------------
-    // Format the warning string.
-    //--------------------------------------------------------------------------
-
-    string wrn;
-    wrn += "$(lightpurple)WARNING $(yellow)";
-    wrn.catv( format, va );
-    wrn += "$(off)";
-
-    //--------------------------------------------------------------------------
-    // Translate to CRT escape sequences.
-    //--------------------------------------------------------------------------
-
-    string crt = wrn;
-    translateCrtColors( crt );
-    IEngine::getStdout() += crt + "\n";
-
-    //--------------------------------------------------------------------------
-    // Send to standard out, clear coloration if debuggingg.
-    //--------------------------------------------------------------------------
-
-    stripCrtColors( wrn );
-    return fprintf( stdout, "%s\n", wrn.c_str() );
-  }
-
-//}:                                              |
-//e_errorf:{                                      |
-
-  s32 e_errorf( const u32 code, ccp format,... ){
-    va_list va;
-    va_start( va, format );
-      const s32 r = e_errorv( code, format, va );
-    va_end( va );
-    return r;
-  }
-
-//}:                                              |
-//e_errorv:{                                      |
-
-  s32 e_errorv( const u32 code, ccp format, va_list va ){
-
-    //--------------------------------------------------------------------------
-    // Format the warning string.
-    //--------------------------------------------------------------------------
-
-    string err;
-    err += "$(lightred)ERROR $(yellow)";
-    err.catv( format, va );
-    err += "$(off)";
-
-    //--------------------------------------------------------------------------
-    // Translate to CRT escape sequences.
-    //--------------------------------------------------------------------------
-
-    string crt = err;
-    translateCrtColors( crt );
-    IEngine::getStdout() += crt + "\n";
-
-    //--------------------------------------------------------------------------
-    // Send to standard out, clear coloration if debuggingg.
-    //--------------------------------------------------------------------------
-
-    if( IEngine::isDebugging() ){
-      stripCrtColors( err );
+    void e_stktrc( const std::function<void( EON::ccp )>& lambda ){
+      if( !lambda ){
+        return;
+      }
+      auto buffer = (vp*)e_malloc( 1024*sizeof( vp )); // NB:Guaranteed to succeed.
+      auto n_ptrs = backtrace( buffer, 1024 );
+      auto strngs = backtrace_symbols(
+          buffer
+        , n_ptrs );
+      if( strngs ){
+        for( auto n=n_ptrs, i=0; i<n; ++i ){
+          lambda( strngs[ i ]);
+        }
+      }
+      e_free( vp( buffer ));
     }
-    OutputDebugString( err + "\n" );
-    fprintf( stdout, err + "\n"  );
-  DEBUG_BREAK
-    return err.len();
-  }
 
-//}:                                              |
-//e_dbgf:{                                        |
+  //}:                                            |
+  //e_warnsv:{                                    |
 
-  s32 e_dbgf( ccp format, ... ){
-    va_list va;
-    va_start( va, format );
-      const s32 r=( 0 != e_errorv( 999, format, va )||1 );
-    va_end( va );
-    return r;
-  }
+    s32 e_warnsv( ccp format, va_list va ){
 
-//}:                                              |
-//e_msgf:{                                        |
+      //------------------------------------------------------------------------
+      // Format the warning string.
+      //------------------------------------------------------------------------
 
-  s32 e_msgf( ccp format,... ){
-    va_list va;
-    va_start( va, format );
-      const s32 r = e_msgv( format, va );
-    va_end( va );
-    return r;
-  }
+      string wrn = "  $(lightpurple)WARNING $(yellow)";
+      wrn.catv( format, va );
+      wrn += "$(off)";
 
-//}:                                              |
-//e_msgf:{                                        |
+      //------------------------------------------------------------------------
+      // Translate to CRT escape sequences.
+      //------------------------------------------------------------------------
 
-  s32 e_msgv( ccp format, va_list va ){
+      string crt = wrn;
+      string con = crt;
+      string::asCLI( crt );
+      IEngine::getStdout() += crt + "\n";
 
-    //--------------------------------------------------------------------------
-    // Format the warning string.
-    //--------------------------------------------------------------------------
+      //------------------------------------------------------------------------
+      // Send to standard out, clear coloration if debuggingg.
+      //------------------------------------------------------------------------
 
-    string msg;
-    msg.catv( format, va );
-    msg += "$(off)";
-
-    //--------------------------------------------------------------------------
-    // Translate to CRT escape sequences.
-    //--------------------------------------------------------------------------
-
-    string crt = msg + "\n";
-    IEngine::getStdout() += crt;
-
-    //--------------------------------------------------------------------------
-    // Send to standard out, clear coloration if debuggingg.
-    //--------------------------------------------------------------------------
-
-    stripCrtColors( crt );
-    OutputDebugString( crt );
-    fprintf( stdout, crt );
-    return msg.len();
-  }
-
-//}:                                              |
-//e_logf:{                                        |
-
-  s32 e_logf( ccp format,... ){
-    va_list va;
-    va_start( va, format );
-      const s32 r = e_logv( format, va );
-    va_end( va );
-    return r;
-  }
-
-//}:                                              |
-//e_logv:{                                        |
-
-  s32 e_logv( ccp format, va_list va ){
-    std::lock_guard lockGuard( mutie() );
-    const auto& msg = e_xfs( format, va );
-    OutputDebugString( msg );
-    OutputDebugString( "\n" );
-    fprintf( stdout, msg + "\n"  );
-    return 1;
-  }
-
-//}:                                              |
-//e_log:{                                         |
-
-  s32 e_log( ccp msg ){
-    if( msg && *msg ){
-      std::lock_guard lockGuard( mutie() );
-      OutputDebugString( msg );
-      OutputDebugString( "\n" );
-      fprintf( stdout, string( msg ) + "\n"  );
-      return s32( strlen( msg ));
+      string::strip( crt );
+      return fprintf( stdout
+        , "%s\n"
+        , ccp( crt )
+      );
     }
-    return 0;
-  }
 
+  //}:                                            |
+  //e_warnsf:{                                    |
+
+    s32 e_warnsf( ccp format,... ){
+      va_list va;
+      va_start( va, format );
+        const s32 r = e_warnsv( format, va );
+      va_end( va );
+      return r;
+    }
+
+  //}:                                            |
+  //e_breakf:{                                    |
+
+    s32 e_breakf( ccp format,... ){
+      va_list va;
+      va_start( va, format );
+          string breakf;
+          breakf.catv(
+              format
+            , va );
+      va_end( va );
+      e_break(
+        breakf
+      );
+    }
+
+  //}:                                            |
+  //e_break:{                                     |
+
+    void e_break( ccp msg ){
+      static auto isBroke = false;
+      if( !isBroke ){
+        puts( "" );
+        puts( "***************" );
+        puts( "*    BREAK    *" );
+        puts( "***************" );
+      }
+      if( msg )
+        printf( "  [%u]  %s\n", Thread::tid(), msg );
+      if( e_getCvar<bool>( "USE_BUILT_IN_TRAP"_64 ))
+        __builtin_trap();
+      string _msg( msg );
+      string _stk;
+      const auto&__stk = boost::stacktrace::stacktrace();
+      const auto&__vec = __stk.as_vector();
+      for( auto n=__vec.size(), i=0ul; i<n; ++i ){
+        const auto& t = __vec[ i ];
+        _stk << t.name().c_str() << "\n";
+      }
+      e_throwGpuAlert(
+          _msg
+        , _stk
+      );
+    }
+
+  //}:                                            |
+  //e_msgv:{                                      |
+
+    s32 e_msgv( ccp format, va_list va ){
+
+      //------------------------------------------------------------------------
+      // Format the warning string.
+      //------------------------------------------------------------------------
+
+      string msg;
+      msg.catv( format, va );
+      auto con( msg );
+      msg += "$(off)";
+      auto crt( msg );
+
+      //------------------------------------------------------------------------
+      // Translate to CRT escape sequences.
+      //------------------------------------------------------------------------
+
+      IEngine::getStdout() += string::asCLI( crt );
+      IEngine::getStdout() += "\n";
+
+      //------------------------------------------------------------------------
+      // Send to standard out, clear coloration if debuggingg.
+      //------------------------------------------------------------------------
+
+      if( e_isDebugging() )
+        string::strip( con );
+      const auto l = fprintf( stdout
+        , "%s\n"
+        , ccp( con ));
+      const string path( "~/.eon/run.log" );
+      auto* f = fopen( path.os(), "a" );
+      if( f ){
+        #if!e_compiling( debug )
+          string::strip( con );
+        #endif
+        fprintf( f, "%s\n", ccp( con ));
+        fclose( f );
+        f = nullptr;
+      }
+      return l;
+    }
+
+  //}:                                            |
+  //e_logf:{                                      |
+
+    s32 e_logf( ccp format,... ){
+      va_list va;
+      va_start( va, format );
+        const s32 r = e_logv( format, va );
+      va_end( va );
+      return r;
+    }
+
+  //}:                                            |
+  //e_logv:{                                      |
+
+    s32 e_logv( ccp format, va_list va ){
+      const auto l=vprintf( format, va );
+      if( !l )
+        return 0;
+      printf( "\n" );
+      return l+1;
+    }
+
+  //}:                                            |
+  //e_log:{                                       |
+
+    s32 e_log( ccp msg ){
+      if( msg && *msg ){
+        puts( msg );
+        return s32( strlen( msg ));
+      }
+      return 0;
+    }
+
+  //}:                                            |
+  //e_halt:{                                      |
+
+    void e_halt( ccp msg ){
+      puts( "**************" );
+      puts( "*    HALT    *" );
+      puts( "**************" );
+      if( msg ){
+        printf( "  [%u]  %s\n", Thread::tid(), msg );
+      }
+      if( e_isDebugging() &&
+             e_getCvar<bool>( "_BREAK_INTO_CODE"_64 )){
+        __builtin_trap();
+      }else{
+        exit( -1 );
+      }
+    }
+
+  //}:                                            |
+  //e_msgf:{                                      |
+
+    s32 e_msgf( ccp format,... ){
+      va_list va;
+      va_start( va, format );
+        const s32 r = e_msgv( format, va );
+      va_end( va );
+      return r;
+    }
+
+  //}:                                            |
+  //e_msg:{                                       |
+
+    s32 e_msg( ccp text ){
+
+      //------------------------------------------------------------------------
+      // Format the warning string.
+      //------------------------------------------------------------------------
+
+      string msg( text );
+      auto con( msg );
+      msg += "$(off)";
+      auto crt( msg );
+
+      //------------------------------------------------------------------------
+      // Translate to CRT escape sequences.
+      //------------------------------------------------------------------------
+
+      if( e_isDebugging() ){
+        string::strip( con );
+      }
+      string::asCLI( crt );
+      IEngine::getStdout() += crt + "\n";
+
+      //------------------------------------------------------------------------
+      // Send to standard out, clear coloration if debuggingg.
+      //------------------------------------------------------------------------
+
+      const auto l = fprintf( stdout
+        , "%s\n"
+        , ccp( con ));
+      const string path( "~/.eon/run.log" );
+      auto* f = fopen( path.os(), "a" );
+      if( f ){
+        #if!e_compiling( debug )
+          string::strip( con );
+        #endif
+        fprintf( f, "%s\n", ccp( con ));
+        fclose( f );
+        f = nullptr;
+      }
+      return l;
+    }
+
+  //}:                                            |
 //}:                                              |
-//e_brk:{                                         |
-
-  void e_brk( ccp msg ){
-    std::lock_guard lockGuard( mutie() );
-    OutputDebugString( "***************\n" );
-    OutputDebugString( "*    BREAK    *\n" );
-    OutputDebugString( "***************\n" );
-    if( msg ){
-      OutputDebugString( e_xfs( "[%u] %s\n", Thread::tid(), msg ));
-    }
-    if( IEngine::isDebugging() ){
-      DebugBreak();
-    }
-    if( msg ){
-      e_errorf( 999, "[%u] %s", Thread::tid(), msg );
-    }
-    exit( -1 );
-  }
-
-//}:                                              |
-//e_hlt:{                                         |
-
-  void e_hlt( ccp msg ){
-    std::lock_guard lockGuard( mutie() );
-    OutputDebugString( "**************\n" );
-    OutputDebugString( "*    HALT    *\n" );
-    OutputDebugString( "**************\n" );
-    if( msg ){
-      OutputDebugString( e_xfs( "[%u] %s\n", Thread::tid(), msg ));
-    }
-    if( IEngine::isDebugging() ){
-      DebugBreak();
-    }
-    if( msg ){
-      e_errorf( 999, "[%u] %s", Thread::tid(), msg );
-    }
-    exit( -1 );
-  }
-
-//}:                                              |
-//================================================|=============================
+//================================================+=============================
+//                                                                 vim:ft=objcpp
