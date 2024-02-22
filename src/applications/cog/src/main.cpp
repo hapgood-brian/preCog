69c7afc71a8f6319f08eeed8a9308f31a70a0ada
Lord Shader <creepydollsoft@gmail.com>
2024-02-22 15:43:40 -0600

    Upped version.

--- src/applications/cog/src/main.cpp
+++ src/applications/cog/src/main.cpp
@@ -968,7 +968,7 @@ using namespace fs;
         // Each has 256 steps: 0x00 thru 0xFF.
         static constexpr u8 major = 0x02; // Major version number [majrelease]
         static constexpr u8 minor = 0x00; // Minor version number [minrelease]
-        static constexpr u8 rev   = 0x03; // Revision
+        static constexpr u8 rev   = 0x04; // Revision
         static constexpr u8 build = 0x00; // Build
 
         //----------------------------------------------------------------------
