029a3d02917c92221151f975a76e1d91c9ff720c
Lord Shader <creepydollsoft@gmail.com>
2024-02-22 18:49:13 -0600

    New release v2.0.5

--- bin/macOS/pkgCreator.sh
+++ bin/macOS/pkgCreator.sh
@@ -1,1 +1,1 @@
-pkgbuild --root . --identifier com.creepydollgames.precog --version 2.0.3 --install-location /usr/local/bin precog.pkg
+pkgbuild --root . --identifier com.creepydollgames.precog --version 2.0.5 --install-location /usr/local/bin precog.pkg
